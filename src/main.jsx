372ed46d024740a1efe1f4aa58fb106a8d22f74a
Baxriddin Chorshanbiyev <baxriddin.dev@gmail.com>
2023-11-18 15:18:16 +0500

    FIX: some bugs

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import App from './App.jsx'
 // Font styles
 import "@fontsource/inter";
 import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/900.css";
 
 const root = createRoot(document.getElementById('root'))
 
