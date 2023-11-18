11f4b95c416d220038606272add958842c15def9
Baxriddin Chorshanbiyev <baxriddin.dev@gmail.com>
2023-11-18 17:46:12 +0500

    FINISH: AvitoInfo section in Home page

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -4,7 +4,7 @@ import { Container } from "../common";
 
 export default function Footer() {
   return (
-    <Flex bg="#FAFAFA" h="71px" justify="center" align="center">
+    <Flex bg="#FAFAFA" h="71px" justify="center" align="center" fontSize="14px">
       <Container>
         <Flex justify="space-between" align="center" gap="20px">
           <Text>@ 2023 Avito Plus - ИП Иванов Иван Иванович ИНН 7729773587</Text>
@@ -14,7 +14,7 @@ export default function Footer() {
                 Политика конфиденциальности
               </Text>
             </Link>
-            <Link to="/Oferta">
+            <Link to="/oferta">
               <Text _hover={{ textDecoration: "underline" }}>
                 Оферта
               </Text>
