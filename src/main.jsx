07bc344e82641baa99092b4f3e34b8f3d48b6987
Baxriddin Chorshanbiyev <baxriddin.dev@gmail.com>
2023-11-18 15:05:07 +0500

    ADDED: Font family

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
 import App from './App.jsx'
 
+// Font styles
+import "@fontsource/inter";
+import "@fontsource/inter/400.css";
+
 const root = createRoot(document.getElementById('root'))
 
 const theme = extendTheme({
